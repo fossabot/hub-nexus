db05348f78af6776c77b91553b18702c4251168e
Brian Mandel <bamandel@gmail.com>
2017-07-26 18:22:03 -0400

    Added an additional dependency

---
+++ src/main/groovy/com/blackducksoftware/integration/hub/nexus/application/HelloWorld.groovy
@@ -0,0 +1,23 @@
+package com.blackducksoftware.integration.hub.nexus.application
+/*
+ * Copyright (C) 2017 Black Duck Software Inc.
+ * http://www.blackducksoftware.com/
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Black Duck Software ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Black Duck Software.
+ */
+
+
+
+class HelloWorld {
+
+    public void test() {
+    }
+
+    static main(args) {
+    }
+}
