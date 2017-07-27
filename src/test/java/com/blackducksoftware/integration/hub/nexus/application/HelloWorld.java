6df335f16c0579a077dc7f33c22b152a4d60fc34
Brian Mandel <bamandel@gmail.com>
2017-07-27 16:46:48 -0400

    First tries

---
+++ src/test/java/com/blackducksoftware/integration/hub/nexus/application/HelloWorld.java
@@ -0,0 +1,66 @@
+/*
+ * hub-nexus
+ *
+ * 	Copyright (C) 2017 Black Duck Software, Inc.
+ * 	http://www.blackducksoftware.com/
+ *
+ * 	Licensed to the Apache Software Foundation (ASF) under one
+ * 	or more contributor license agreements. See the NOTICE file
+ * 	distributed with this work for additional information
+ * 	regarding copyright ownership. The ASF licenses this file
+ * 	to you under the Apache License, Version 2.0 (the
+ * 	"License"); you may not use this file except in compliance
+ * 	with the License. You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * 	Unless required by applicable law or agreed to in writing,
+ * 	software distributed under the License is distributed on an
+ * 	"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * 	KIND, either express or implied. See the License for the
+ * 	specific language governing permissions and limitations
+ * 	under the License.
+ */
+package com.blackducksoftware.integration.hub.nexus.application;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.eclipse.sisu.EagerSingleton;
+import org.sonatype.nexus.plugin.PluginIdentity;
+
+/*
+ * Copyright (C) 2017 Black Duck Software Inc.
+ * http://www.blackducksoftware.com/
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Black Duck Software ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Black Duck Software.
+ */
+
+@Named
+@EagerSingleton
+public class HelloWorld extends PluginIdentity {
+
+    public static final String ID_PREFIX = "helloworld";
+
+    public static final String GROUP_ID = "com.blackducksoftware.integrations";
+
+    public static final String ARTIFACT_ID = "blackducksoftware-" + ID_PREFIX + "-plugin";
+
+    public static final String REST_PREFIX = "/" + ID_PREFIX;
+
+    public static final String PERMISSION_PREFIX_LOGGERS = "blackducksoftware:logconfig:";
+
+    public static final String PERMISSION_PREFIX_LOG = "blackducksoftware:logs:";
+
+    @Inject
+    public HelloWorld() throws Exception {
+        super(GROUP_ID, ARTIFACT_ID);
+        this.log.info("HelloWorld, ctor");
+    }
+
+}
---
+++ src/test/java/com/blackducksoftware/integration/hub/nexus/application/HelloWorldTaskDescriptor.java
@@ -0,0 +1,53 @@
+/*
+ * hub-nexus
+ *
+ * 	Copyright (C) 2017 Black Duck Software, Inc.
+ * 	http://www.blackducksoftware.com/
+ *
+ * 	Licensed to the Apache Software Foundation (ASF) under one
+ * 	or more contributor license agreements. See the NOTICE file
+ * 	distributed with this work for additional information
+ * 	regarding copyright ownership. The ASF licenses this file
+ * 	to you under the Apache License, Version 2.0 (the
+ * 	"License"); you may not use this file except in compliance
+ * 	with the License. You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * 	Unless required by applicable law or agreed to in writing,
+ * 	software distributed under the License is distributed on an
+ * 	"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * 	KIND, either express or implied. See the License for the
+ * 	specific language governing permissions and limitations
+ * 	under the License.
+ */
+package com.blackducksoftware.integration.hub.nexus.application;
+
+import java.util.List;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.sonatype.nexus.formfields.FormField;
+import org.sonatype.nexus.tasks.descriptors.AbstractScheduledTaskDescriptor;
+
+@Named(HelloWorldTaskDescriptor.ID)
+@Singleton
+public class HelloWorldTaskDescriptor extends AbstractScheduledTaskDescriptor {
+    public static final String ID = "NexusHelloWorld";
+
+    @Override
+    public String getId() {
+        return ID;
+    }
+
+    @Override
+    public String getName() {
+        return "HELLO WORLD!";
+    }
+
+    @Override
+    public List<FormField> formFields() {
+        return null;
+    }
+}
