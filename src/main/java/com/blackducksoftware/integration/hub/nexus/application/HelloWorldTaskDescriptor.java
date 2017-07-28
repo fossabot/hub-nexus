281284da42e18a3a0ea04c216e403f055abf0719
Brian Mandel <bamandel@gmail.com>
2017-07-28 14:43:32 -0400

    Fixed a build error

--- src/main/java/com/blackducksoftware/integration/hub/nexus/application/HelloWorldTaskDescriptor.java
+++ src/main/java/com/blackducksoftware/integration/hub/nexus/application/HelloWorldTaskDescriptor.java
@@ -1,25 +1,25 @@
 /*
  * hub-nexus
  *
- * Copyright (C) 2017 Black Duck Software, Inc.
- * http://www.blackducksoftware.com/
+ * 	Copyright (C) 2017 Black Duck Software, Inc.
+ * 	http://www.blackducksoftware.com/
  *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * 	Licensed to the Apache Software Foundation (ASF) under one
+ * 	or more contributor license agreements. See the NOTICE file
+ * 	distributed with this work for additional information
+ * 	regarding copyright ownership. The ASF licenses this file
+ * 	to you under the Apache License, Version 2.0 (the
+ * 	"License"); you may not use this file except in compliance
+ * 	with the License. You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * 	http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * 	Unless required by applicable law or agreed to in writing,
+ * 	software distributed under the License is distributed on an
+ * 	"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * 	KIND, either express or implied. See the License for the
+ * 	specific language governing permissions and limitations
+ * 	under the License.
  */
 package com.blackducksoftware.integration.hub.nexus.application;
 
@@ -54,7 +54,6 @@ public class HelloWorldTaskDescriptor extends AbstractScheduledTaskDescriptor {
         final CheckboxFormField checkbox = new CheckboxFormField("cbId", "Checkbox test", "This field is for testing the check boxes", false);
         final TextAreaFormField textarea = new TextAreaFormField("taId", "text area test", "This field is for testing the text area", false);
         final List<FormField> list = new ArrayList<>();
-        list.add(test);
         list.add(checkbox);
         list.add(textarea);
         return list;
