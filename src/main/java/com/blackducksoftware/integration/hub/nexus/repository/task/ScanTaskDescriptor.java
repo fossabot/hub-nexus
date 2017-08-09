1e3863b06a21fb1417189e051dddd75372326ec8
Paulo Santos <psantos@blackducksoftware.com>
2017-08-09 12:45:37 -0400

    Add the working directory as a location to install the CLI

--- src/main/java/com/blackducksoftware/integration/hub/nexus/repository/task/ScanTaskDescriptor.java
+++ src/main/java/com/blackducksoftware/integration/hub/nexus/repository/task/ScanTaskDescriptor.java
@@ -40,6 +40,7 @@ import org.sonatype.nexus.tasks.descriptors.AbstractScheduledTaskDescriptor;
 @Singleton
 public class ScanTaskDescriptor extends AbstractScheduledTaskDescriptor {
     private static final String DEFAULT_FILE_PATTERNS = "*.war,*.zip,*.tar.gz,*.hpi";
+    private static final String DEFAULT_WORKING_DIRECTORY = "/sonatype-work";
     private static final String DEFAULT_HUB_TIMEOUT = "300";
     public static final String ID = "Hub Repository Scan";
 
@@ -62,19 +63,21 @@ public class ScanTaskDescriptor extends AbstractScheduledTaskDescriptor {
         final StringTextFormField resourceStorePathField = new StringTextFormField(TaskField.REPOSITORY_PATH_FIELD_ID.getParameterKey(), "Repository path",
                 "Enter a repository path to run the task in recursively (ie. \"/\" for root or \"/org/apache\").", FormField.OPTIONAL);
         final StringTextFormField hubUrlField = new StringTextFormField(TaskField.HUB_URL.getParameterKey(), "Hub URL", "URL to your Blackduck hub", FormField.MANDATORY);
-        final StringTextFormField usernameField = new StringTextFormField(TaskField.HUB_USERNAME.getParameterKey(), "Hub username", "Username for your Blackduck hub account to properly connect", FormField.MANDATORY);
-        final PasswordFormField passwordField = new PasswordFormField(TaskField.HUB_PASSWORD.getParameterKey(), "Hub password", "Password for your Blackduck hub account to properly connect", FormField.MANDATORY);
+        final StringTextFormField usernameField = new StringTextFormField(TaskField.HUB_USERNAME.getParameterKey(), "Hub Username", "Username for your Blackduck hub account to properly connect", FormField.MANDATORY);
+        final PasswordFormField passwordField = new PasswordFormField(TaskField.HUB_PASSWORD.getParameterKey(), "Hub Password", "Password for your Blackduck hub account to properly connect", FormField.MANDATORY);
         final StringTextFormField timeoutField = new StringTextFormField(TaskField.HUB_TIMEOUT.getParameterKey(), "Timeout", "The timeout in seconds for a request to the Blackduck Hub server", FormField.OPTIONAL)
                 .withInitialValue(DEFAULT_HUB_TIMEOUT);
         final CheckboxFormField autoImportCert = new CheckboxFormField(TaskField.HUB_AUTO_IMPORT_CERT.getParameterKey(), "Auto Import Certs", "Auto-import Hub server certificate into keystore", FormField.OPTIONAL);
 
         final StringTextFormField proxyHostField = new StringTextFormField(TaskField.HUB_PROXY_HOST.getParameterKey(), "Proxy Host", "The hostname of the proxy to communicate with the Blackduck Hub", FormField.OPTIONAL);
         final StringTextFormField proxyPortField = new StringTextFormField(TaskField.HUB_PROXY_PORT.getParameterKey(), "Proxy Port", "Port to communicate with the proxy", FormField.OPTIONAL);
-        final StringTextFormField proxyUsernameField = new StringTextFormField(TaskField.HUB_PROXY_USERNAME.getParameterKey(), "Proxy username", "Username for your authenticated proxy", FormField.OPTIONAL);
-        final PasswordFormField proxyPasswordField = new PasswordFormField(TaskField.HUB_PROXY_PASSWORD.getParameterKey(), "Proxy password", "Password for your authenticated proxy", FormField.OPTIONAL);
+        final StringTextFormField proxyUsernameField = new StringTextFormField(TaskField.HUB_PROXY_USERNAME.getParameterKey(), "Proxy Username", "Username for your authenticated proxy", FormField.OPTIONAL);
+        final PasswordFormField proxyPasswordField = new PasswordFormField(TaskField.HUB_PROXY_PASSWORD.getParameterKey(), "Proxy Password", "Password for your authenticated proxy", FormField.OPTIONAL);
 
         final StringTextFormField filePatternField = new StringTextFormField(TaskField.FILE_PATTERNS.getParameterKey(), "File Pattern Matches", "The file pattern match wildcard to filter the artifacts scanned.", FormField.MANDATORY)
                 .withInitialValue(DEFAULT_FILE_PATTERNS);
+        final StringTextFormField workingDirectoryField = new StringTextFormField(TaskField.WORKING_DIRECTORY.getParameterKey(), "Working Directory",
+                "The parent directory where the blackduck directory will be creating to contain temporary data for the scans", FormField.MANDATORY).withInitialValue(DEFAULT_WORKING_DIRECTORY);
 
         fields.add(repoField);
         fields.add(resourceStorePathField);
@@ -88,6 +91,7 @@ public class ScanTaskDescriptor extends AbstractScheduledTaskDescriptor {
         fields.add(proxyUsernameField);
         fields.add(proxyPasswordField);
         fields.add(filePatternField);
+        fields.add(workingDirectoryField);
 
         return fields;
     }
