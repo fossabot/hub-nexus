428a8757c23c606a78f400af0095d7d18e6a9c9b
Paulo Santos <psantos@blackducksoftware.com>
2017-08-17 08:10:53 -0400

    add a script to help uploading artifacts

---
+++ src/test/resources/upload_artifacts.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ARTIFACT=$1
+ITERATIONS=$2
+
+if [ -z "$ARTIFACT" ]; then
+    echo "No artifact path specified exiting..."
+    exit 1
+fi
+
+if [ -z "$ITERATIONS" ]; then
+    ITERATIONS=5
+fi
+
+for i in `seq 1 $ITERATIONS`;
+do
+    curl -v -F "r=releases" -F "g=com.blackducksoftware.test$i" -F "a=test" -F "v=$i.0.0" -F "p=zip" -F "file=@$ARTIFACT" -u admin:admin123 http://localhost:8081/nexus/service/local/artifact/maven/content
+done
