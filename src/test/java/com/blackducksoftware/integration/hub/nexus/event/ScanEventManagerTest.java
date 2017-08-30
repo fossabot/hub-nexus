43949ad3e64417fb2ef558711e4fe3d0218522fe
Paulo Santos <psantos@blackducksoftware.com>
2017-08-30 12:00:14 -0400

    Implement the unit test for the scan event manager

--- src/test/java/com/blackducksoftware/integration/hub/nexus/event/ScanEventManagerTest.java
+++ src/test/java/com/blackducksoftware/integration/hub/nexus/event/ScanEventManagerTest.java
@@ -23,6 +23,94 @@
  */
 package com.blackducksoftware.integration.hub.nexus.event;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.sonatype.nexus.events.Event;
+import org.sonatype.nexus.proxy.item.RepositoryItemUid;
+import org.sonatype.nexus.proxy.item.StorageItem;
+import org.sonatype.nexus.proxy.repository.Repository;
+
+import com.blackducksoftware.integration.hub.nexus.helpers.TestEventBus;
+
 public class ScanEventManagerTest {
 
+    private static String TEST_TASK_NAME = "IntegationTestTask";
+
+    private TestEventBus eventBus;
+    private ScanEventManager eventManager;
+    private StorageItem item;
+
+    @Before
+    public void init() {
+        eventBus = new TestEventBus();
+        eventManager = new ScanEventManager(eventBus);
+        item = Mockito.mock(StorageItem.class);
+        final Repository repository = Mockito.mock(Repository.class);
+        final RepositoryItemUid repositoryUid = Mockito.mock(RepositoryItemUid.class);
+        Mockito.when(repositoryUid.getRepository()).thenReturn(repository);
+        Mockito.when(item.getRepositoryItemUid()).thenReturn(repositoryUid);
+    }
+
+    @After
+    public void cleanUp() {
+        eventBus.removeAllEvents();
+    }
+
+    @Test
+    public void testProcessItemNullParameterMap() throws Exception {
+        final ScanItemMetaData data = new ScanItemMetaData(item, null, null);
+        eventManager.processItem(data);
+        assertFalse(eventBus.hasEvents());
+    }
+
+    @Test
+    public void testProcessItem() throws Exception {
+        final Map<String, String> taskParameters = new HashMap<>();
+        taskParameters.put(ScanEventManager.PARAMETER_KEY_TASK_NAME, TEST_TASK_NAME);
+        final ScanItemMetaData data = new ScanItemMetaData(item, null, taskParameters);
+        eventManager.processItem(data);
+        assertTrue(eventBus.hasEvents());
+    }
+
+    @Test
+    public void testPendingEventCount() throws Exception {
+        assertEquals(0, eventManager.pendingEventCount(TEST_TASK_NAME));
+        final Map<String, String> taskParameters = new HashMap<>();
+        taskParameters.put(ScanEventManager.PARAMETER_KEY_TASK_NAME, TEST_TASK_NAME);
+        final ScanItemMetaData data = new ScanItemMetaData(item, null, taskParameters);
+        eventManager.processItem(data);
+        assertTrue(eventBus.hasEvents());
+        assertEquals(eventBus.getEventCount(), eventManager.pendingEventCount(TEST_TASK_NAME));
+    }
+
+    @Test
+    public void testMarkEventProcessed() throws Exception {
+        assertEquals(0, eventManager.pendingEventCount(TEST_TASK_NAME));
+        final Map<String, String> taskParameters = new HashMap<>();
+        taskParameters.put(ScanEventManager.PARAMETER_KEY_TASK_NAME, TEST_TASK_NAME);
+        final ScanItemMetaData data = new ScanItemMetaData(item, null, taskParameters);
+        eventManager.processItem(data);
+        assertTrue(eventBus.hasEvents());
+        assertEquals(eventBus.getEventCount(), eventManager.pendingEventCount(TEST_TASK_NAME));
+
+        final Collection<Event<?>> eventCollection = eventBus.getEvents();
+        for (final Event<?> event : eventCollection) {
+            if (event instanceof HubScanEvent) {
+                final HubScanEvent scanEvent = (HubScanEvent) event;
+                eventManager.markScanEventProcessed(scanEvent);
+                assertTrue(scanEvent.isProcessed());
+            }
+        }
+        assertEquals(0, eventManager.pendingEventCount(TEST_TASK_NAME));
+    }
 }
